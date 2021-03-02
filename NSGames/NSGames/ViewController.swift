18f48b35b1969ca1388103eeb6011f055cde3b47
Nikita Sosyuk <nikitashelov@gmail.com>
2021-03-02 14:45:52 +0300

    Initial commit

---
+++ NSGames/NSGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NSGames
+//
+//  Created by Nikita Sosyuk on 02.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
